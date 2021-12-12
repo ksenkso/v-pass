f9c5a31709f45c37fde443d1f2c9a8149906eb2b
ksenkso <yazunya@gmail.com>
2021-12-12 23:28:08 +0300

    chore(build): minor upgrades:
    * Add .gitignore
    * Add eslint-ignore for `new Vue`
    * Add package-lock.json
    * Add new line at the end of the package.json

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import JsonTree from "vue-json-tree";
 Vue.component("json-tree", JsonTree);
 
 Vue.config.productionTip = false;
+// eslint-disable-next-line no-unused-vars
 const app = new Vue({
   el: "#app",
   render: (h) => h(App)
